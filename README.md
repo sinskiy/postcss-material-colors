7914eb6e036ca7d534a6e0b486851909c9d00042
sinskiy <sinskiy@proton.me>
2024-08-31 17:41:22 +0300

    feat: apply only at AtRules

--- README.md
+++ README.md
@@ -4,30 +4,35 @@ A PostCSS plugin to use the Material Design 3 Color System with PostCSS.
 
 ## usage
 
+1.
+
 ```bash
 npm install --save-dev postcss-material-colors
 ```
 
-in postcss config:
+2. in postcss config:
 
 ```js
 import theme from "postcss-material-colors"
 
 ...
   plugins: [
-    theme({
-      "#ff0000", // primary color
-      "tonalSpot", // variant, optional (default shown)
-       0.0, // contrast, optional (default shown)
-    }),
+    theme(),
     ...
   ],
 ...
+```
+
+3. in css (usually your root file):
+
+```css
+@postcss-material-colors (primary: "#ff0000", variant: "tonalSpot", contrast: 0.0);
 ```
 
 [More about args](https://github.com/material-foundation/material-color-utilities/blob/main/dev_guide/creating_color_scheme.md)
 
 ## soon
 
-- custom colors
+- extra colors
 - dark mode support for .dark
+- typescript
--- index.js
+++ index.js
@@ -3,11 +3,29 @@ import { getTheme } from "./theme.js";
 /**
  * @type {import('postcss').PluginCreator}
  */
-const plugin = (opts) => {
-  const { light, dark } = getTheme(opts);
+const plugin = () => {
   return {
     postcssPlugin: "postcss-material-colors",
     Once(root, { Declaration }) {
+      const atRuleOpts = {};
+
+      root.walkAtRules("postcss-material-colors", (rule) => {
+        const opts = rule.params.slice(1, -1).split(",");
+        if (!opts.length) {
+          throw new Error("You must provide at least primary color");
+        }
+
+        for (const opt of opts) {
+          const [key, value] = opt.split(":").map((value) => value.trim());
+          if (!key || !value) {
+            throw new Error("You must provide key and value separated by :");
+          }
+          atRuleOpts[key] = value;
+        }
+      });
+
+      const { light, dark } = getTheme(atRuleOpts);
+
       root.prepend({ selector: ":root" });
 
       for (const colorName in light) {
